ea9844c725f0d93c8a3e1abddf70bc23464f40fd
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-02-26 18:24:32 -0500

    Switcher from Dark to Light

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,10 +1,12 @@
 
 import CopyInput from './components/CopyInput' 
+import Switcher from './components/Switcher'
 
 function App() {
 
   return <div>
     <CopyInput/>
+    <Switcher/>
   </div>
 }
 
