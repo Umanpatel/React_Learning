8efe7cd0ae8b13922f9f9c52c42b9dca0360ed63
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-03-01 17:33:49 -0500

    useRef Hook (Timer & Focus on Input Field)

---
+++ useRef-hook/src/App.jsx
@@ -0,0 +1,13 @@
+import FocusInput from "./components/focusInput"
+import Timer from "./components/Timer"
+
+const App = () => {
+  return (
+    <div>
+      <FocusInput/>
+      <Timer/>
+    </div>
+  )
+}
+
+export default App
